173c6f0aa487f10564077d37f952a4d83f0e97f8
ajtelesnavarro <ajtelesnavarro@gmail.com>
2026-04-17 16:54:18 -0300

    added new images and updated the CSS and PHP files to reflect the changes in the product listings and supplier details. The commit includes modifications to the product quantities, addition of new products, and updates to the supplier information. The changes aim to enhance the user experience by providing more accurate and comprehensive data on the products and suppliers available in the system.

--- PROJETO CONSTRUTECH/PHP/produtos.php
+++ PROJETO CONSTRUTECH/PHP/produtos.php
@@ -13,6 +13,7 @@
     require 'init.php';
 
     $categoria_get = isset($_GET['categoria']) ? trim($_GET['categoria']) : '';
+    $busca_get = isset($_GET['busca']) ? trim($_GET['busca']) : '';
 
     ?>
     <section class="conteudo">
@@ -26,12 +27,15 @@
                 ?>
             </ul>
         </div>
+        <form method="GET" class="busca_marca_produto">
+            <input type="text" name="busca" placeholder="Pesquisar produtos ou marcas...">
+            <button type="submit">Pesquisar</button>
+        </form>
         <table class="container_conteudo_planilha_produto">
             <tr class="container_planilha_produto_especificações">
                 <th>ID</th>
                 <th>NOME</th>
                 <th>MARCA</th>
-                <th>QTD MÍNIMA</th>
                 <th>QTD ATUAL</th>
                 <th>PREÇO UN</th>
                 <th>PREÇO TOTAL</th>
@@ -41,7 +45,13 @@
             $total_preco_total = 0;
             $preco_total = 0;
             foreach ($_SESSION['produtos'] as $produto) {
-                if ($categoria_get === '' || $produto['categoria'] === $categoria_get) {
+                $filtro_categoria = ($categoria_get === '' || $produto['categoria'] === $categoria_get);
+                $filtro_busca = (
+                    $busca_get === '' ||
+                    strpos(strtolower($produto['nome']), strtolower($busca_get)) !== false ||
+                    strpos(strtolower($produto['marca']), strtolower($busca_get)) !== false
+                );
+                if ($filtro_categoria && $filtro_busca) {
                     $total_qtd_atual += $produto['quantidade_atual'];
                     $preco_total = $produto['preco_unitario'] * $produto['quantidade_atual'];
                     $total_preco_total += $preco_total;
@@ -49,9 +59,8 @@
                         print '
                     <tr class="container_planilha_produto_dados">
                         <td>' . $produto['id'] . '</td>
-                        <td><a href="especificacoes_produtos.php?id='. $produto['id'] . '">' . $produto['nome'] . '</a></td>
+                        <td><a href="especificacoes_produtos.php?id=' . $produto['id'] . '">' . $produto['nome'] . '</a></td>
                         <td>' . $produto['marca'] . '</td>
-                        <td>' . $produto['quantidade_minima'] . ' UN</td>
                         <td class="texto_vermelho">' . $produto['quantidade_atual'] . ' UN</td>
                         <td>R$' . number_format($produto['preco_unitario'], 2, ',', '.') . '</td>
                         <td>R$' . number_format($preco_total, 2, ',', '.') . '</td>
@@ -60,9 +69,8 @@
                         print '
                     <tr class="container_planilha_produto_dados">
                         <td>' . $produto['id'] . '</td>
-                        <td><a href="especificacoes_produtos.php?id='. $produto['id'] . '">' . $produto['nome'] . '</a></td>
+                        <td><a href="especificacoes_produtos.php?id=' . $produto['id'] . '">' . $produto['nome'] . '</a></td>
                         <td>' . $produto['marca'] . '</td>
-                        <td>' . $produto['quantidade_minima'] . ' UN</td>
                         <td>' . $produto['quantidade_atual'] . ' UN</td>
                         <td>R$' . number_format($produto['preco_unitario'], 2, ',', '.') . '</td>
                         <td>R$' . number_format($preco_total, 2, ',', '.') . '</td>
@@ -73,7 +81,7 @@
             }
             ?>
             <tr class="container_planilha_produto_rodape">
-                <td colspan="4">TOTAL</td>
+                <td colspan="3">TOTAL</td>
                 <td><?php print $total_qtd_atual; ?> UN</td>
                 <td></td>
                 <td>R$<?php print number_format($total_preco_total, 2, ',', '.'); ?></td>
