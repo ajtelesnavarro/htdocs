2e0071cfffe7e1029f068832248e197387bbd93f
ajtelesnavarro <ajtelesnavarro@gmail.com>
2026-05-11 16:28:14 -0300

    começo do proejto integrador

---
+++ PROJETO INTEGRADOR/TESTES/PHP/index.php
@@ -0,0 +1,47 @@
+<head>
+    <title>Teste PHP</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+
+<body>
+    <header>
+        <a class="logotipo">
+            <img><!-- simbolo png
+            texto img -->
+        </a>
+        <div class="menu-direita">
+            <ul>
+                <li><a>
+                        <p><!-- sobre nós --></p>
+                    </a></li>
+            </ul>
+
+            <ul>
+                <li><a>
+                        <p><!-- máquinas --></p>
+                    </a></li>
+            </ul>
+
+            <ul>
+                <li><a>
+                        <p><!-- contato --></p>
+                    </a></li>
+            </ul>
+
+            <button><!-- login --></button>
+
+        </div>
+    </header>
+    <section class="conteudo">
+        <div class="img-direita">
+            <img><!-- imagem --></img>
+        </div>
+        <div class="img-esquerda">
+            <img><!-- imagem --></img>
+        </div>
+        
+        <div class="texto-slogan">
+            <h1><!-- slogan --></h1>
+        </div>
+    </section>
+</body>
